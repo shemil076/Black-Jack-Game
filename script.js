0c4180c5931d5dd33c993206fc3a2c766a308dec
Pramuditha Karunarathna <shemil076@gmail.com>
2022-05-15 22:39:52 +0530

    Implemented to generate the total

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function generateCard(){
+   
     let x;
     do{
     x = Math.floor((Math.random() *100 /7.6 )+1)
    }while(x == 0)
+
+   let currentTotal = document.getElementById("result").innerHTML;
+   let total = parseInt(currentTotal) + x;
+
+    document.getElementById("result").innerHTML = total;
+
     var img = document.createElement("img")
     img.src = "images/" + x + ".png"
     img.height = "300"
