0b22af293e1c61dd02ba558c32849d743b787234
Pramuditha Karunarathna <shemil076@gmail.com>
2022-05-15 22:34:45 +0530

    Fixed the 0 bug

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function generateCard(){
-    let x = Math.floor((Math.random() *100 /7.6 )+1)
+    let x;
+    do{
+    x = Math.floor((Math.random() *100 /7.6 )+1)
+   }while(x == 0)
     var img = document.createElement("img")
     img.src = "images/" + x + ".png"
     img.height = "300"
     document.body.appendChild(img)
-    
+
 }
