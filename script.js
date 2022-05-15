8a8aa5b783d3d7df034df51ae03d63f42096852d
Pramuditha Karunarathna <shemil076@gmail.com>
2022-05-15 22:30:28 +0530

    Generate random cards

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function generateCard(){
     let x = Math.floor((Math.random() *100 /7.6 )+1)
-    alert(x)
+    var img = document.createElement("img")
+    img.src = "images/" + x + ".png"
+    img.height = "300"
+    document.body.appendChild(img)
+    
 }
